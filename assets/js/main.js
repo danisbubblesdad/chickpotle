96fcf118af2298dfad78b9e4f241b32d0335fa1c
Daniel Crittenden <dan.crittenden@gmail.com>
2017-07-25 15:47:21 -0400

    Added customized map and marker functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,60 @@ function showPage() {
   document.getElementById("mainContent").style.display = "block";
 }
 
+
+////////// maps
+
+function initMap() {
+
+  var userLatitude = parseFloat(localStorage.getItem("latitude"));
+  var userLongitude  = parseFloat(localStorage.getItem("longitude"));
+  var userLocation = {lat: userLatitude, lng: userLongitude};
+  var chickfilaLocation = chickpotle.chickfila_location;
+  var chipotleLocation = chickpotle.chipotle_location;
+
+  var bounds = new google.maps.LatLngBounds();
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: userLocation
+  });
+
+// Set the marker for the user position
+
+  var userMarker = new google.maps.Marker({
+    position: userLocation,
+    map: map,
+    title: "Your location"
+  });
+
+// Set the chick-fil-a marker
+
+  var chickfilaMarker = new google.maps.Marker({
+    position: chickfilaLocation,
+    map: map,
+    title: "Chick-fil-A"
+  });
+
+// Set the chipotle marker
+
+  var chipotleMarker = new google.maps.Marker({
+    position: chipotleLocation,
+    map: map,
+    title: "Chipotle"
+  });
+
+// Adjust zoom to all markers  
+
+  bounds.extend(userMarker.position);
+  bounds.extend(chipotleMarker.position);
+  bounds.extend(chickfilaMarker.position);
+  map.fitBounds(bounds);
+
+
+
+
+}
+
 //////////// Geolocation
 
 
@@ -116,5 +170,6 @@ findUserLocation.then()
   //available properties are: addresses, IDs, and trip duration (in seconds)
   chickpotle = results[0];
   console.log(chickpotle);
+  initMap();
   showPage();
 })
